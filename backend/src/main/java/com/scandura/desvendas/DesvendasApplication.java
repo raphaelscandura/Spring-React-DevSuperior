d474fc55cabf886f6d1929721ad844dde489e653
raphaelscandura <raphaelscandura@hotmail.com>
2021-09-06 13:34:13 -0300

    First commit, project created

---
+++ backend/src/main/java/com/scandura/desvendas/DesvendasApplication.java
@@ -0,0 +1,13 @@
+package com.scandura.desvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/scandura/desvendas/DesvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scandura.desvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
