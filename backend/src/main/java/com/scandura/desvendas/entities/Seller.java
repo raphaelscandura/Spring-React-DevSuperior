9a58aca7c2054ad566ea08c2a771add7fb1a5949
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-08 18:09:41 -0300

    Modelo de dominio, seed do banco de dados

---
+++ backend/src/main/java/com/scandura/desvendas/entities/Seller.java
@@ -0,0 +1,53 @@
+package com.scandura.desvendas.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "seller")
+    private List<Sale> sales = new ArrayList<>();
+
+    public Seller(){
+    }
+    
+    public Seller(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public List<Sale> getSales(){
+        return sales;
+    }
+
+}
