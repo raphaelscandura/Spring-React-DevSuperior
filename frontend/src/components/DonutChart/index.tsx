608c51c56e1157e9d0569e6a49085bf5c6cce12c
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-10 10:53:23 -0300

    Integração do DonutChart

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -8,29 +9,33 @@ type ChartData = {
     labels: string[];
 }
 
-const DonutChart = () =>{
+const DonutChart = () => {
 
-    let chartData : ChartData = {labels: [], series: []};
+    //Manter estado no componente
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x =>  x.sellerName);
-            const mySeries = data.map(x => x.sum);
-            chartData = {labels: myLabels, series:mySeries};
-        })
+    //Executar algo na instanciação ou destruição do componente, observar estado
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
 
     const options = {
         legend: {
             show: true
         }
     }
-    return(
+    return (
         <Chart
-            options={{ ...options, labels: chartData.labels}}
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
-            height = "240"
+            height="240"
         />
 
     );
